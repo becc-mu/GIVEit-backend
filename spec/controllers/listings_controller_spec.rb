b2c74334f703b37b875ba5c53e781f2abe770200
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-11 15:49:35 +0000

    Fix test for listings controller GET #index

--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -3,22 +3,17 @@ require 'rails_helper'
 RSpec.describe ListingsController, type: :controller do
   describe "GET #index" do
     it "returns a JSON list of the listings" do
-      listing = Listing.create! {
-        {
-          title: 'Listing Title',
-          description: 'Description',
-          start_date: 'Start Date',
-          end_date: 'End Date',
-          listing_type: 'Listing Type',
-          location: 'Location'
-        }
-      }
+      listing = Listing.create({
+        title: 'Listing Title',
+        description: 'Description',
+        start_date: 'Start Date',
+        end_date: 'End Date',
+        listing_type: 'Listing Type',
+        location: 'Location'
+      })
 
       get :index, params: {}
       expect(response).to be_successful
-
-      p listing
-      p JSON.parse(response.body)
 
       expect(JSON.parse(response.body)).to eq([
         {
