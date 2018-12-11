c9534a97e17d611c8b1914bdcf186ef8653d4b84
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-11 11:28:33 +0000

    Add test for listing model

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
+  it "can create a valid listing" do
+    valid_listing = Listing.new(
+      title: 'Listing Title',
+      description: 'Description',
+      start_date: 'Start Date',
+      end_date: 'End Date',
+      listing_type: 'Listing Type',
+      location: 'Location'
+    )
+
+    expect(valid_listing).to be_valid
+  end
 end
