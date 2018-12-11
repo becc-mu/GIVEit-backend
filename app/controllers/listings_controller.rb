9879ff6ed7bae749652df5ea2673884763cd8d4b
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-11 11:29:32 +0000

    Add test for listings controller and update listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,8 @@
 class ListingsController < ApplicationController
   def index
+    @listings = Listing.all
+
+    render json: @listings
   end
 
   def new
--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -1,16 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe ListingsController, type: :controller do
-  let(:valid_attributes) {
-    {  }
-  }
-
   describe "GET #index" do
     it "returns a JSON list of the listings" do
-      listing = Listing.create! valid_attributes
+      listing = Listing.create! {
+        {
+          title: 'Listing Title',
+          description: 'Description',
+          start_date: 'Start Date',
+          end_date: 'End Date',
+          listing_type: 'Listing Type',
+          location: 'Location'
+        }
+      }
 
       get :index, params: {}
       expect(response).to be_successful
+
+      p JSON.parse(response.body)
+
+      expect(JSON.parse(response.body)).to eq([
+        {
+          "id" => listing.id,
+          "title" => listing.title,
+          "description" => listing.description,
+          "start_date" => listing.start_date,
+          "end_date" => listing.end_date,
+          "listing_type" => listing.listing_type,
+          "location" => listing.location
+        }
+        ])
     end
   end
 end
