4d3487a261154fe58c771fbca946f11b8a162713
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-12 10:21:04 +0000

    Refactored listings controller spec to use valid_listing_attributes helper

--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe ListingsController, type: :controller do
+  let(:valid_listing_attributes) {
+    {
+      title: 'Listing Title',
+      description: 'Description',
+      start_date: 'Start Date',
+      end_date: 'End Date',
+      listing_type: 'Listing Type',
+      location: 'Location'
+    }
+  }
+
   describe "GET #index" do
     it "returns a JSON list of the listings" do
-      listing = Listing.create({
-        title: 'Listing Title',
-        description: 'Description',
-        start_date: 'Start Date',
-        end_date: 'End Date',
-        listing_type: 'Listing Type',
-        location: 'Location'
-      })
+      listing = Listing.create(valid_listing_attributes)
 
       get :index, params: {}
       expect(response).to be_successful
@@ -33,14 +37,7 @@ RSpec.describe ListingsController, type: :controller do
 
   describe "GET #show" do
     it "returns JSON of the listing" do
-      listing = Listing.create({
-        title: 'Listing Title',
-        description: 'Description',
-        start_date: 'Start Date',
-        end_date: 'End Date',
-        listing_type: 'Listing Type',
-        location: 'Location'
-      })
+      listing = Listing.create(valid_listing_attributes)
       get :show, params: {id: listing.to_param}
       expect(response).to be_successful
       expect(JSON.parse(response.body)).to eq(
@@ -63,28 +60,12 @@ RSpec.describe ListingsController, type: :controller do
     context "with valid params" do
       it "creates a new listing" do
         expect {
-          post :create, params: { listing: {
-              title: 'Listing Title',
-              description: 'Description',
-              start_date: 'Start Date',
-              end_date: 'End Date',
-              listing_type: 'Listing Type',
-              location: 'Location'
-            }
-          }
+          post :create, params: { listing: valid_listing_attributes }
         }.to change(Listing, :count).by(1)
       end
 
       it "renders a JSON response with new listing" do
-        post :create, params: {listing: {
-            title: 'Listing Title',
-            description: 'Description',
-            start_date: 'Start Date',
-            end_date: 'End Date',
-            listing_type: 'Listing Type',
-            location: 'Location'
-          }
-        }
+        post :create, params: { listing: valid_listing_attributes }
         expect(response).to have_http_status(:created)
         expect(response.content_type).to eq('application/json')
         expect(response.location).to eq(listing_url(Listing.last))
@@ -110,7 +91,7 @@ RSpec.describe ListingsController, type: :controller do
 
     context "with invalid params" do
       it "renders a JSON response with errors for the listing" do
-        
+
       end
     end
   end
