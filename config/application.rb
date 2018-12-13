aaad513245ae085ea3de092bbcb4d0625696bc2c
Muna <mmhussein125@gmail.com>
2018-12-13 11:32:55 +0000

    CORS configuration

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,14 @@ Bundler.require(*Rails.groups)
 
 module GIVEitBackend
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
