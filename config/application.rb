fde62918b17c8dad34914d2b18aafe05d122024a
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-13 17:37:44 +0000

    change to allow * origins

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module GIVEitBackend
   class Application < Rails::Application
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins '*'
         resource '*', headers: :any, methods: %i[get post options]
       end
     end
